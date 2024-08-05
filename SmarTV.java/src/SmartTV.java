fe2080021e7b1b9a33cef4adb820a327219052a1
Endrie Cassemiro <endrie97@gmail.com>
2024-08-05 13:54:47 -0300

    Projeto SmartTv

---
+++ SmarTV.java/src/SmartTV.java
@@ -0,0 +1,34 @@
+public class SmartTV {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void mudarCanal (int novoCanal){
+        canal = novoCanal;
+    }
+    public void aumentarCanal(){
+        canal++;
+    }
+
+    public void diminunirCanal(){
+        canal--;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando o Volume para: " + volume);
+    }
+    
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo o Volume para: " + volume);
+    }
+
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+}
